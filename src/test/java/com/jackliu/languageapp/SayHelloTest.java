6a57acc52adb411c1b9a79754a40dcac3f929e45
obrutjack <obrutjack@yahoo.com>
2023-08-15 22:45:59 +0800

    Create Java project for language app.

---
+++ src/test/java/com/jackliu/languageapp/SayHelloTest.java
@@ -0,0 +1,11 @@
+package com.jackliu.languageapp;
+
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+  
+public class SayHelloTest {
+    @Test
+    public void testSayHello() throws IOException {
+        SayHello.main(new String[]{"en"});
+    } 
+}
